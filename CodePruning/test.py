52a2b072aae4706703f5813a033a513a7a5fff0a
R-Bread <81481533+R-Bread@users.noreply.github.com>
2021-04-12 01:39:50 +0530

    Update test.py

--- CodePruning/test.py
+++ CodePruning/test.py
@@ -3,19 +3,18 @@
 # THESE COMMENTS, AS WELL AS ALL UNUSED FUNCTIONS/CLASSES SHOULD BE REMOVED.
 
 def func(x):
-	"""This function is kinda useless."""
+	"""This function is used in the code."""
 	return x
 
 def func2(x):
-	'''DOCSTRING, YEAH'''
+	'''This one is kinda useless.'''
 	return 5
 
 # func2 should be removed, as it won't be used.
 
 class Real:
 	isReal = True
-	# func2 = 5
-	# ^ For extra challenge later.
+	# Random comment.
 
 	def __init__(self,x,y):
 		self.x = x
@@ -26,7 +25,6 @@ class Real:
 
 class Fake:
 	isReal = False
-	# func2 = 5
 
 	def __init__(self,x,y):
 		self.x = x
