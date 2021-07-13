de751c273a50bd2a8adc821b429a215cb20287dd
tulip <tulip@cse.iitb.ac.in>
2021-07-14 01:27:15 +0530

    checked layer2

--- Keyword_sequence_comparision/ksc.py
+++ Keyword_sequence_comparision/ksc.py
@@ -1,6 +1,24 @@
 import nltk
 import string
-from .LCS import lcs
+
+def lcs(a, b):
+    tbl = [[0 for B in range(len(b) + 1)] for A in range(len(a) + 1)]
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            tbl[i + 1][j + 1] = tbl[i][j] + 1 if x == y else max(
+                tbl[i + 1][j], tbl[i][j + 1])
+    res = []
+    i, j = len(a), len(b)
+    while i and j:
+        if tbl[i][j] == tbl[i - 1][j]:
+            i -= 1
+        elif tbl[i][j] == tbl[i][j - 1]:
+            j -= 1
+        else:
+            res.append(a[i - 1])
+            i -= 1
+            j -= 1
+    return res[::-1]
 
 def ksc(code1,code2):
     C_Keywords = ['auto', 'double', 'int', 'struct', 'break', 'else', 'long', 'switch', 'case', 'enum', 'register', 'typedef', 'char', 'extern', 'return', 'union', 'continue', 'for', 'signed', 'void', 'do', 'if', 'static', 'while', 'default', 'goto', 'sizeof', 'volatile', 'const', 'float', 'short', 'unsigned' ]
@@ -17,15 +35,8 @@ def ksc(code1,code2):
     for line in code2:
         lines2 += line.strip()
 
-
-    Lines1 = nltk.sent_tokenize(lines1)
-    Lines2 = nltk.sent_tokenize(lines2)
-
-    for line in Lines1:
-        words1 += nltk.word_tokenize(line)
-
-    for line in Lines2:
-        words2 += nltk.word_tokenize(line)
+    words1 += nltk.word_tokenize(lines1)
+    words2 += nltk.word_tokenize(lines2)
 
     #print(lines1)
     #print(lines2)
@@ -48,6 +59,10 @@ def ksc(code1,code2):
 
     a = len(LCS_kw)
     b = max(len(keyword_sequence1),len(keyword_sequence2))
-
+    if b == 0 :
+        return 100
     matching = (a/b)*100
     return matching
+
+if __name__ == '__main__':
+    print(ksc(open(r'./Source_code1.txt','r'),open(r'./Source_code2.txt','r')))
