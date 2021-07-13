3a61e4c076533c6ed8506fef2649b31bac9b8430
tulip <tulip@cse.iitb.ac.in>
2021-07-14 00:56:25 +0530

    checked layer1

--- Final/Multi_Layer_Comparison.py
+++ Final/Multi_Layer_Comparison.py
@@ -1,39 +1,29 @@
 import sys
 sys.path.append('../')
 
-from typing import Sequence
-import numpy as np
-from array import *
-import subprocess
-import re
-import nltk
-import string
-from collections import Counter
 
 from Indentation.indent import indentComparison
 from Function_Signature.Function_Signature import functionSignatureComp
 from Variable_and_Operator_Count.Variable_and_Operator_count import varAndOperCount
+from Keyword_sequence_comparision.ksc import ksc
 
-def multiLayerComparison(file_name1,file_name2, key=0):
+def multiLayerComparison(file_name1,file_name2):
     func_layer1 = [indentComparison, functionSignatureComp, varAndOperCount]
     th_layer1 = [30, 30, 30]
 
-    # Reading the files
-    f1 = open(file_name1, 'r')
-    f2 = open(file_name2, 'r')
+    func_layer2 = [ksc]
 
-    percents_layer1 = [fun(f1, f2) for fun in func_layer1]
+    percents_layer1 = [fun(open(file_name1, 'r'), open(file_name2, 'r')) for fun in func_layer1]
+    percents_layer2 = [fun(open(file_name1, 'r'), open(file_name2, 'r')) for fun in func_layer2]
+
     print(percents_layer1)
-
-    # Close the files
-    f1.close()
-    f2.close()
+    print(percents_layer2)
 
     return 60
 
 if __name__ == "__main__":
-    fname1=r'./dataset/B2016_Z1_Z1/student5420.cpp'                              #Path for original file
-    fname2=r'./dataset/B2016_Z1_Z1/student5533.cpp'                              #Path for file that is being checked for plagiarism (copy)
+    fname1=r'./Dataset/B2016_Z1_Z1/student5420.cpp'                              #Path for original file
+    fname2=r'./Dataset/B2016_Z1_Z1/student5533.cpp'                              #Path for file that is being checked for plagiarism (copy)
 
     print("Percentage plag confidence = ", multiLayerComparison(fname1, fname2))
 
--- Variable_and_Operator_Count/Variable_and_Operator_count.py
+++ Variable_and_Operator_Count/Variable_and_Operator_count.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 # To count the operators
 def countOperators(s, dict):
@@ -15,10 +16,41 @@ def countOperators(s, dict):
             dict[op] = len(re.findall(pat, s))
         else:
             dict[op] = len(re.findall('\\'+'\\'.join(op), s))
+    
 
 # To count the variable types
 def countVariables(s, dict):
-    pass
+    s = re.sub('[^_]int32|unsigned int|[^n]signed int','int',s)
+    s = re.sub('[^_]int64|long long|long long int','long',s)
+    dataTypes = ['int','float','char','short','long','double']
+    counts = {'int':0,'float':0, 'char':0, 'short':0, 'long':0, 'double':0, 'int1d':0, 'float1d':0, 'char1d':0, 'short1d':0, 'long1d':0, 'double1d':0, 'int2d':0, 'float2d':0, 'char2d':0, 'short2d':0, 'long2d':0, 'double2d':0}
+    lines = re.split('\n|;|{|}|\(|\)',s)
+    for line in lines:
+        if(line):
+            line = line.strip(' ')
+            for dataType in dataTypes:
+                if(line.startswith(dataType)):
+                    dimension = line.count('[')
+                    if(dimension == 0):
+                        counts[dataType] += (line.count(',')+1)
+                    else:
+                        if(line.count(',') == 0):
+                            if(dimension == 1):
+                                counts[dataType+'1d'] += 1
+                            else:
+                                if(dimension == 2):
+                                    counts[dataType+'2d'] += 1
+                        else:
+                            words = line.split(',')
+                            for word in words:
+                                dim = word.count('[')
+                                if(dim == 1):
+                                    counts[dataType+'1d'] += 1
+                                else:
+                                    if(dim == 2):
+                                        counts[dataType+'2d'] += 1
+    for k,v in counts.items():
+        dict[k] = v
 
 # Function to make dictionary with var and operator counts
 def makeDict(f):
@@ -44,17 +76,27 @@ def makeDict(f):
 
     countOperators(s, dict)
     s = re.sub('\((.*?)\)', '()', s)
-    print(s)
-    print('=============================')
     countVariables(s, dict)
     return dict
 
 def varAndOperCount(f1, f2):
     dict1 = makeDict(f1)
     dict2 = makeDict(f2)
-    print(dict1)
     # Write logic to find percent similarity
-    return 60
+    cnt = 0
+    sz = 0
+    for k,v1 in dict1.items():
+        v2 = dict2[k]
+        va = max(v1,v2)
+        vb = min(v1,v2)
+        if(va == 0):
+            continue
+        sz += 1
+        if((va-vb) <= math.ceil(0.1*va)):
+            cnt += 1
+
+    ret_val = (cnt/sz)*100
+    return ret_val
 
 if __name__ == "__main__":
     fname1=r'./original.cpp'                              #Path for original file
