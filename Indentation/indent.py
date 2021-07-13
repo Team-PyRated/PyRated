8290cddf78cf1f76cb019ed502281d30621cc35e
vibhav011 <vibhav200601@gmail.com>
2021-07-13 18:38:49 +0530

    Checked indent.py and added __init__.py

--- Indentation/indent.py
+++ Indentation/indent.py
@@ -3,7 +3,6 @@ This code compares two text files for similarity based on the degree of indent m
 fname1, fname2 are the two files being compared. Change path accordingly
 
 '''
-import numpy as np
 from array import *
 
 def lcs(seq1 , seq2):
@@ -17,54 +16,54 @@ def lcs(seq1 , seq2):
         for iter2 in range(n2+1):
             if iter1 == 0 or iter2 == 0 :
                 L[iter1][iter2] = 0
-            elif seq1[iter1-1] == seq2[iter2-1]:                                #If elements match
+            elif abs(seq1[iter1-1] - seq2[iter2-1]) < 3:                 #If elements match or are close to each other
                 L[iter1][iter2] = 1+L[iter1-1][iter2-1]
             else:
                 L[iter1][iter2] = max(L[iter1-1][iter2] , L[iter1][iter2-1])    #If elements do not match
     return L[n1][n2]                                                            #Return length of LCS
 
-fname1=r'./original.txt'                              #Path for original file
-fname2=r'./copy.txt'                                  #Path for file that is being checked for plagiarism (copy)
+# For generationg the array containinbg the size of indentation for each line
+def generateArray(f):
+    space = []
+    for line1 in f:
+        ct=0
+        
+        for char1 in line1:
+            if char1==' ':
+                ct+=1
+            elif char1 == '\t':
+                ct+=4
+            else:
+                break
+        space.append(ct)
+    return space
 
-#Read files
-f1=open(fname1,'r')
-f2=open(fname2,'r')
+def indentComparison(f1, f2):
+    #Array containing size of indent for each line 
+    space1=generateArray(f1)
+    space2=generateArray(f2)
 
-#Array containing size of indent for each line 
-space1=[]
-space2=[]
+    # #Print sequence of indents for both files
+    # print(space1)
+    # print(space2)
 
-#Generate indent size sequence for original file
-for line1 in f1:
-    ct=0
-    index=0
+    if len(space1) + len(space2) == 0:
+        return 100
     
-    for char1 in line1:
-        if char1==' ':
-            ct+=1
-        else:
-            break
-    space1.append(ct)
-    index+=1
+    return 200*(lcs(space1,space2))/(len(space1) + len(space2))                           #Calculate Percentage indent match
 
-#Generate indent size sequence for copy file
-for line2 in f2:
-    ct=0
-    index=0
-    
-    for char2 in line2:
-        if char2==' ':
-            ct+=1
-        else:
-            break
-    space2.append(ct)
-    index+=1
 
-#Print sequence of indents for both files
-print(space1)
-print(space2)
+if __name__ == "__main__":
+    fname1=r'./original.txt'                              #Path for original file
+    fname2=r'./copy.txt'                                  #Path for file that is being checked for plagiarism (copy)
 
-indentmatch=lcs(space1,space2)/len(space1)                                          #Calculate Percentage indent match
-print("Percentage indent match = ",100*indentmatch)
+    # Read the files
+    f1 = open(fname1, 'r')
+    f2 = open(fname2, 'r')
 
-    
+    print("Percentage indent match = ",indentComparison(f1, f2))
+
+    # Close the files
+    f1.close()
+    f2.close()
+
