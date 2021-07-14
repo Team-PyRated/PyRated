37b7e2de1b3b4a2629996f0f9d05b0ba0a44bd1a
vibhav011 <vibhav200601@gmail.com>
2021-07-15 03:24:52 +0530

    Added AST in multi_layer_comparison

--- Final/Multi_Layer_Comparison.py
+++ Final/Multi_Layer_Comparison.py
@@ -6,18 +6,22 @@ from Indentation.indent import indentComparison
 from Function_Signature.Function_Signature import functionSignatureComp
 from Variable_and_Operator_Count.Variable_and_Operator_count import varAndOperCount
 from Keyword_sequence_comparision.ksc import ksc
+from AST.ASTcombine import ASTmatch
 
 def multiLayerComparison(file_name1,file_name2):
     func_layer1 = [indentComparison, functionSignatureComp, varAndOperCount]
     th_layer1 = [30, 30, 30]
 
     func_layer2 = [ksc]
+    func_layer3 = [ASTmatch]
 
     percents_layer1 = [fun(open(file_name1, 'r'), open(file_name2, 'r')) for fun in func_layer1]
     percents_layer2 = [fun(open(file_name1, 'r'), open(file_name2, 'r')) for fun in func_layer2]
+    percents_layer3 = [fun(open(file_name1, 'r'), open(file_name2, 'r')) for fun in func_layer3]
 
     print(percents_layer1)
     print(percents_layer2)
+    print(percents_layer3)
 
     return 60
 
