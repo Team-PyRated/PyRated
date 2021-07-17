0a35ff6f2a885b94834c5cb16dd684b2ca771df6
Bread <81481533+R-Bread@users.noreply.github.com>
2021-07-17 17:05:40 +0530

    Update Multi_Layer_Comparison.py

--- FINAL/Multi_Layer_Comparison.py
+++ FINAL/Multi_Layer_Comparison.py
@@ -1,5 +1,7 @@
 import re
 
+
+from Fucntions.BagOfWords import BOWComparison
 from Functions.indent import indentComparison
 from Functions.Function_Signature import functionSignatureComp
 from Functions.Variable_And_Operator_Count import varAndOperCount
@@ -7,24 +9,35 @@ from Functions.exe_comparison import exe_comp
 from Functions.ksc import ksc
 from Functions.AST_comp import ASTmatch
 
-def multiLayerComparison(file_name1,file_name2):
+def multiLayerComparison(file_name1,file_name2, key=0):
     '''
     Pass file locations starting from the same directory as this functions's file.
     Return True for plagiarised, False for non-plagiarised. If some test not done, None instead of percentage for that test.
+    If key != 0, a list returned - [result, percentage_returned_from_test_1, ... (all test functions)]
     If threshold crossed at any test, then file passed forwards, else declared non-plagiarised.
     If all tests fail to be executed (give some error), None returned.
-    Test order: Indentation Comparison, Variable & Operator Count Comparison, Function Signature, Exe Comparison, Keyword Sequence Comparison, AST Comparison.'''
+    Test order: BOWComparison, Indentation Comparison, Variable & Operator Count Comparison, Function Signature, Exe Comparison, Keyword Sequence Comparison, AST Comparison.'''
 
     perc_list, pass_list = [None for i in range(6)], [None for i in range(6)]
-    
+    func_layer0 = [BOWComparison]
     func_layer1 = [indentComparison, varAndOperCount, functionSignatureComp]
-    func_layer2 = [exe_comp,ksc, ASTmatch]
-    size = [len(func_layer1), len(func_layer2)]
+    func_layer2 = [exe_comp,ksc]
+    func_layer3 = [ASTmatch]
+    size = [len(func_layer0),len(func_layer1), len(func_layer2), len(func_layer3)]
     
-    thresholds = [80,80,75,90,90,90]
+    thresholds = [65,65,65,75,85,85,45]
     
+    perc_list, pass_list = [None for i in range(sum(size))], [None for i in range(sum(size))]
+    def key_result(plag): # Used to return value as per key. 'plag' should be True/False
+        if key==0:
+            return plag
+        else:
+            l = [plag]
+            l.extend(perc_list)
+            return l
     
     ## LAYER 0
+    #  String Comparison
     with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
         # Removing comments:
         text1 = re.sub(r"(//.*?$)|(/\*.*?\*\\)","",f1.read(), flags = re.DOTALL|re.MULTILINE)
@@ -32,50 +45,73 @@ def multiLayerComparison(file_name1,file_name2):
         if text1 == text2:
             return True
     
-    ## LAYER 1
-    for i in range(size[0]):
-        try:
-            with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
-                perc_list[i] = func_layer1[i](f1, f2)
-            if perc_list[i]>thresholds[i]:
-                pass_list[i] = True
-            else:
-                pass_list[i] = False
-        except:
-            print(f"Error in function #{i} in Layer 1")
-        
-    print(perc_list[:size[0]])
+    #  Bag Of Words Comparison
+    try:
+        with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
+            perc_list[0] = func_layer0[0](f1, f2)
+        if perc_list[0] > thresholds[0]:
+            pass_list[0] = True
+    except:
+        print("Error in BOW_comp")
+    
     if any(perc_list[:size[0]]):
         pass
     elif perc_list[:size[0]] == [None for i in range(size[0])]:
         pass
     else:
+        return key_result(False)
+    
+    
+    ## LAYER 1
+    for i in range(size[1]):
+        j = i + size[0]
+        try:
+            with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
+                perc_list[j] = func_layer1[i](f1, f2)
+            if perc_list[j]>thresholds[j]:
+                pass_list[j] = True
+            else:
+                pass_list[j] = False
+        except:
+            print(f"Error in function #{i} in Layer 1")
+        
+    if any(perc_list[ size[0] : sum(size[:1]) ] ):
+        pass
+    else:
         return False
+    
     
     ## LAYER 2
     #  EXE COMPARISON
     # ENTER FILENAME INSTEAD OF OPEN() OBJECT.
+    i = sum(size[:1])
     try:
-        perc_list[size[0]] = func_layer2[0](file_name1, file_name2)
-        if perc_list[size[0]] > thresholds[size[0]]:
-            pass_list[size[0]] = True
+        perc_list[i] = func_layer2[0](file_name1, file_name2)
+        if perc_list[i]] > thresholds[i]:
+            pass_list[i] = True
     except:
         print("Error in exe_comp")
 
-    # KEYWORD SEQUENCE COMPARISON
+    #  KEYWORD SEQUENCE COMPARISON
     try:
         with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
-            perc_list[size[0]+1] = func_layer2[1](f1, f2)
-        if perc_list[size[0]+1] > thresholds[size[0]+1]:
-            pass_list[size[0]+1] = True
+            perc_list[i+1] = func_layer2[1](f1, f2)
+        if perc_list[i+1] > thresholds[i+1]:
+            pass_list[i+1] = True
     except:
         print("Error in ksc")
     
-    # AST COMPARISON
-    i = size[0]+2
+    if any(perc_list[ i : sum(size[:2]) ] ):
+        return key_result(True)
+    else:
+        return key_result(False)
+    
+    ## LAYER 3
+    #  AST COMPARISON
+    i = i+size[2]
     try:
         with open(file_name1, 'r') as f1, open(file_name2, 'r') as f2:
-            perc_list[i] = func_layer2[2](f1, f2)
+            perc_list[i] = func_layer3[0](f1, f2)
         if perc_list[i]>thresholds[i]:
             pass_list[i] = True
         else:
@@ -83,11 +119,10 @@ def multiLayerComparison(file_name1,file_name2):
     except:
         print("Error in AST comparison")
     
-    print(perc_list[ size[0] : size[0]+size[1] ] )
-    if any(perc_list[ size[0] : size[0]+size[1] ] ):
-        return True
+    if any(perc_list[ i : sum(size) ] ):
+        return key_result(True)
     else:
-        return False
+        return key_result(False)
 
 if __name__ == '__main__':
-    print(multiLayerComparison('dataset3/Arithmetic/student1.cpp','dataset3/Arithmetic/student5.cpp'))
+    print(multiLayerComparison('dataset3/Arithmetic/student1.cpp','dataset3/Arithmetic/student5.cpp',1))
